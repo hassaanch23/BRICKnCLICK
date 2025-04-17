b424719b9542564f1dccf42984b5ddf3088ca1ce
samar-2004 <samarjunaid1994@gmail.com>
2025-04-17 17:11:49 +0500

    Server Created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,15 +13,8 @@ export default function Header() {
             <span className="px-1"> CLicK</span>
           </h1>
         </Link>
-        <form className="bg-gray-300 rounded-lg p-2 flex items-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent focus:outline-none rounded-md w-24 sm:w-64 md:w-90 pr-3"
-          />
-          <FaSearch className="text-gray-800" />
-        </form>
-        <ul className="flex gap-4 text-gray-50 md:gap-8 text-sm sm:text-base font-semibold md:mr-10">
+
+        <ul className="flex gap-4 text-gray-50 md:gap-8 text-sm sm:text-base font-semibold ">
           <Link to="/">
             <li className="relative group hidden sm:inline">
               <span className="group-hover:text-orange-400 transition duration-300">
@@ -47,6 +40,16 @@ export default function Header() {
             </li>
           </Link>
         </ul>
+
+        <form className="bg-gray-300 rounded-lg p-2 flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none rounded-md w-24 sm:w-40 md:w-60 pr-3"
+          />
+          <FaSearch className="text-gray-800" />
+        </form>
+      
       </div>
     </header>
   );
