a879e41591eba61eb77e4b1e50856c1885850da8
samar-2004 <samarjunaid1994@gmail.com>
2025-04-19 00:15:08 +0500

    signUp page UI done

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>
-      SignUp
+    <div className="h-[calc(100vh-64px)] overflow-hidden bg-gradient-to-br from-blue-500 to-orange-500 flex justify-center items-center px-4">
+      <div className="w-full max-w-md p-8 bg-white/30 backdrop-blur-md rounded-2xl shadow-xl animate-fade-in-down">
+        <h2 className="text-3xl font-bold text-gray-800 text-center mb-6">
+          Create Your Account
+        </h2>
+
+        <form className="space-y-5">
+          <div>
+            <label
+              className="block text-gray-700 font-medium mb-1"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              id="username"
+              type="text"
+              placeholder="e.g. samar_dev"
+              className="w-full px-4 py-2 rounded-lg bg-white/70 focus:outline-none focus:ring-2 focus:ring-orange-500 transition-all"
+            />
+          </div>
+
+          <div>
+            <label
+              className="block text-gray-700 font-medium mb-1"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="you@example.com"
+              className="w-full px-4 py-2 rounded-lg bg-white/70 focus:outline-none focus:ring-2 focus:ring-orange-500 transition-all"
+            />
+          </div>
+
+          <div>
+            <label
+              className="block text-gray-700 font-medium mb-1"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <div className="relative">
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="••••••••"
+                className="w-full px-4 py-2 pr-10 rounded-lg bg-white/70 focus:outline-none focus:ring-2 focus:ring-orange-500 transition-all"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-2 top-2 text-gray-600 hover:text-blue-500 transition"
+              >
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-3 mt-4 bg-orange-600 text-white font-semibold rounded-lg hover:opacity-80 transition-all shadow-md hover:shadow-lg uppercase cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            Sign Up
+          </button>
+          <p className="text-center gap-2 text-sm text-gray-700 mt-4 font-semibold">
+            Already have an account? 
+            <Link
+              to={"/sign-in"}
+              className="text-blue-700 hover:underline ml-1 font-bold "
+            >
+              Sign in
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
