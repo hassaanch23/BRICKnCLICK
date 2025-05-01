eec0ba4e64d23d15c954d799f06c78e3e0aa5c5d
samar-2004 <samarjunaid1994@gmail.com>
2025-05-02 01:37:10 +0500

    done with edit-listing Api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -71,3 +71,21 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 }
+
+export const  updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return res.status(404).json({ message: 'Listing not found' });
+    }
+    if(req.user.id !== listing.userRef) {
+        return res.status(403).json({ message: 'You are not authorized to update this listing' });
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.status(200).json(updatedListing);    
+    }
+        
+        catch (error) {
+        next(error);}
+
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, uploadImages,deleteListing } from '../controllers/listing.controller.js';
+import { createListing, uploadImages,deleteListing,updateListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../middleware/auth.js';
 import upload from '../config/storage.js';
 
@@ -13,4 +13,5 @@ router.post('/create', verifyToken, createListing);
 
 router.delete('/delete/:id', verifyToken, deleteListing); 
 
+router.post('editListing/:id', verifyToken, updateListing);
 export default router;
