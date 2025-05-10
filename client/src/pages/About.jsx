4f141be6e656c0e4f94ebbfd2306ab12d1d2f8fe
zohaibfast99 <zohaibrashid999@gmail.com>
2025-05-11 01:41:50 +0500

    home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-orange-200">
+      <div className='py-20 px-4 max-w-6xl mx-auto '>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>About Brick&Click</h1>
+      <p className='mb-4 text-slate-700'>
+      Brick & Click is a user-friendly real estate app designed to simplify property searching and management. Our platform connects buyers, sellers, and renters with verified listings and instant communication tools. With real-time updates and smart filters, users enjoy a seamless experience tailored to their property needs.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      Our vision is to revolutionize the real estate experience by making it more transparent, accessible, and efficient. We aim to become a trusted digital platform where people confidently find their next home with just a few clicks, regardless of location or budget.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to empower individuals with innovative tools that enhance decision-making in real estate. By integrating technology, trust, and convenience, we strive to bridge the gap between property seekers and providers—delivering value, saving time, and ensuring a secure and satisfying user journey every step of the way.
+      </p>
+    </div>
     </div>
   )
 }
